41fa599b59a0a7d738658d13c2c27cc413fbaa85
Mariana <mariana@Fauss-MacBook-Pro.local>
2021-10-17 14:22:22 +0200

    Frutas Objetos

--- Fruit.js
+++ Fruit.js
@@ -1,10 +1,27 @@
-var frutas = ["🥝", "🍓", "🍈"];
+var frutas = [
+    {
+        "nombre": "Kiwi",
+        "fruta": "🥝",
+    },
+    {
+        "nombre": "Fresa",
+        "fruta": "🍓",
+    },
+    {
+        "nombre": "Melon",
+        "fruta": "🍈",
+    },
+
+]
 var i = 0;
+
 while (i < frutas.length) {
-    if (frutas[i] == "🥝") {
+    if (frutas[i].fruta == "🥝") {
         console.log("Kiwi");
     }
-    else (console.log(frutas[i]))
+    else {
+        console.log(frutas[i].fruta)
+    }
     i++;
 }
 
