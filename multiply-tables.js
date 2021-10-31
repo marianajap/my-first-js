03721fedb5f68228a47a0c540400db278697afda
Mariana <mariana@Fauss-MacBook-Pro.local>
2021-10-31 12:22:36 +0100

    [multiply-tables.js] display table names

---
+++ multiply-tables.js
@@ -0,0 +1,14 @@
+/**
+ * Muestra en consola las tablas de multiplicar del 0 al 10
+ */
+
+function displayTable(x) {
+    console.log("Table of " + x);
+}
+
+var table = 0;
+
+while(table <= 10) {
+    displayTable(table);
+    table++;
+}
